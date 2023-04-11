85180ae0b27411bf6a0d02401291aecef5d3c6fe
bestemic <przemek.pawlik123@gmail.com>
2023-04-11 19:42:31 +0200

    Added message listener part.

--- zadanie3/src/main/kotlin/com/bestemic/Application.kt
+++ zadanie3/src/main/kotlin/com/bestemic/Application.kt
@@ -1,5 +1,11 @@
 package com.bestemic
 
+import dev.kord.common.entity.Snowflake
+import dev.kord.core.Kord
+import dev.kord.core.event.message.MessageCreateEvent
+import dev.kord.core.on
+import dev.kord.gateway.Intent
+import dev.kord.gateway.PrivilegedIntent
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
@@ -7,24 +13,41 @@ import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
-fun main() {
-    val channelId = "1092841235200344135"
-    val discordClient = DiscordClient("MTA5MjgzOTYzNzk4MDAyNDkxMw.GEuS1v.exSryIc0utuk1N1wQhuJzHS31W5SI_hNo-SEyY")
 
+suspend fun main() {
+    val kord = Kord("token")
+    val channelId = "1092841235200344135"
+
+    kord.on<MessageCreateEvent> {
+        if (message.author?.isBot != false) return@on
+
+        messageHandler(this)
+    }
 
     embeddedServer(Netty, port = 8080) {
-        messageSenderModule(discordClient, channelId)
-    }.start(wait = true)
+        messageSenderModule(kord, channelId)
+    }.start(wait = false)
+
+    kord.login {
+        @OptIn(PrivilegedIntent::class)
+        intents += Intent.MessageContent
+    }
 }
 
-fun Application.messageSenderModule(discordClient: DiscordClient, channelId: String) {
+fun Application.messageSenderModule(kord: Kord, channelId: String) {
     routing {
         post("/send") {
             val message = call.receive<String>()
-            discordClient.sendMessage(channelId, message)
+            kord.rest.channel.createMessage(Snowflake(channelId)) {
+                content = message
+            }
             call.respond("Message was sent to channel with id $channelId.")
         }
     }
+}
+
+fun messageHandler(event: MessageCreateEvent) {
+    println("A message has been received from the user ${event.message.author?.tag}: ${event.message.content}")
 }
 
 
