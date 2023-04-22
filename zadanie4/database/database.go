1e5787019141ec31f32b7ca729d7054902c31b20
bestemic <przemek.pawlik123@gmail.com>
2023-04-22 14:42:54 +0200

    Added product controller.

---
+++ zadanie4/database/database.go
@@ -0,0 +1,19 @@
+package database
+
+import (
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+	"zadanie4/models"
+)
+
+func Connect() *gorm.DB {
+	db, err := gorm.Open(sqlite.Open("data.db"))
+
+	if err != nil {
+		panic("Failed to connect with database")
+	}
+
+	db.AutoMigrate(&models.Product{})
+
+	return db
+}
---
+++ zadanie4/server.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"github.com/labstack/echo/v4"
+	"zadanie4/database"
+	"zadanie4/routes"
+)
+
+func main() {
+	e := echo.New()
+
+	db := database.Connect()
+
+	e.Use(func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			c.Set("db", db)
+			return next(c)
+		}
+	})
+
+	routes.ProductRoute(e)
+
+	e.Logger.Fatal(e.Start(":1323"))
+}
