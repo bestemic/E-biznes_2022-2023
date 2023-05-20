e79a36b1901a48486c465c83ba86adad524f167d
bestemic <przemek.pawlik123@gmail.com>
2023-05-20 23:10:07 +0200

    Added more tests.

--- zadanie6/cypress.config.js
+++ zadanie6/cypress.config.js
@@ -2,8 +2,11 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    // baseUrl: 'https://www.olx.pl',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
+  viewportWidth: 1920,
+  viewportHeight: 1080,
 });
